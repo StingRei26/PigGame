180d7a3f88f596b81410af88534dd29b54eaf3d4
StingRei26 <Reinielravelo@gmail.com>
2018-10-10 07:35:59 -0400

    Code Challenge 3
    Adding the second dice Part 1

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -20,11 +20,17 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
     // 1. Random number
     var  dice = Math.floor(Math.random() * 6 ) + 1; 
+    var  dice2 = Math.floor(Math.random() * 6 ) + 1; 
 
     // 2. Display the resut 
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png'; 
+
+    // Dice two 
+    var diceDom = document.querySelector('.dice2');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice2 + '.png'; 
 
 
     // 3. Update the round score IF the rolled number is NOT a 1
@@ -33,10 +39,11 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     roundScore += dice; 
     document.querySelector('#current-' + activePlayer).textContent = roundScore
      } else {
-
     //Next player 
     nextplayer();
-      }
+    }
+    
+    
     }   
 });
 
@@ -58,6 +65,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 if (scores[activePlayer] >= 20) {
     document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); 
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); 
     gamePlaying = false;
@@ -77,6 +85,7 @@ if (scores[activePlayer] >= 20) {
     document.querySelector('.player-0-panel').classList.toggle('active'); 
     document.querySelector('.player-1-panel').classList.toggle('active'); 
     document.querySelector('.dice').style.display ='none'; 
+    document.querySelector('.dice2').style.display ='none'; 
  };
 
 document.querySelector('.btn-new').addEventListener('click', init); 
@@ -87,7 +96,7 @@ function init(){
     activePlayer = 0; 
     gamePlaying = true;
     document.querySelector('.dice').style.display ='none'; 
-
+    document.querySelector('.dice2').style.display ='none'; 
     document.getElementById('score-0').textContent = '0'; 
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -103,6 +112,21 @@ function init(){
 
 
 
+/*
 
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. 
+(Hint: Always save the previous dice roll in a separate variable)
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. 
+(Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+
+3. Add another dice to the game, so that there are two dices now. 
+The player looses his current score when one of them is a 1. 
+(Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
+*/
 
 
